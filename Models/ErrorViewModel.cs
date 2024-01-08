360f5d510158919b9553cc8dd50640697ebb8952
Diego Martin <diegomartin_000@hotmail.com>
2024-01-08 10:03:50 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chorifest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
